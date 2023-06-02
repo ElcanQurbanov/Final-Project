fdbc2af3e83eaa04b6526fa80c02897c53f389ca
Elcan <eljanng@code.edu.az>
2023-06-02 21:53:24 +0400

    Update Home

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,16 +1,36 @@
-let swiper = new Swiper(".mySwiper", {
-    loop: true,
-    slidesToScroll: 1,
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+// let swiper = new Swiper(".mySwiper", {
+//     loop: true,
+//     slidesToScroll: 1,
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
   
   
-  });
+//   });
+
+
+
+$('.slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 700,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  arrows: false
+});
+
+$(".slider").on("click", function(){
+    $(".slider").slick("slickPlay");
+})
+
+$(".slider").on("mouseover", function(){
+  $(".slider").slick("slickPlay");
+})
