411d4df2e5ef2bb3f84ab703858eb8155d8b5644
Elcan <eljanng@code.edu.az>
2023-06-09 20:48:20 +0400

    Update shop

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,26 +1,7 @@
-// let swiper = new Swiper(".mySwiper", {
-//     loop: true,
-//     slidesToScroll: 1,
-//     spaceBetween: 30,
-//     centeredSlides: true,
-//     autoplay: {
-//       delay: 2500,
-//       disableOnInteraction: false,
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-  
-  
-//   });
-
-
-
 $('.slider').slick({
   dots: false,
   infinite: true,
-  speed: 700,
+  speed: 500,
   fade: true,
   cssEase: 'linear',
   autoplay: true,
