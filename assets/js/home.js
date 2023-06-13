013105414d4471ed96bfbfb3b7daf4d3b24c9a3b
Elcan <eljanng@code.edu.az>
2023-06-13 20:48:11 +0400

    Login and Register finish

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,60 @@
+"use strict";
+
+
+// let userIcon = document.querySelector("#navbar .icons :nth-child(3)");
+
+// userIcon.addEventListener("click", function () {
+
+//   let regDiv = document.querySelector(".login");
+
+//   if(regDiv.classList.contains("d-none")){
+
+//     regDiv.classList.remove("d-none");
+//   }
+//   else{
+//     regDiv.classList.add("d-none");
+//   }
+
+
+// });
+
+
+
+let userIcon = document.querySelector("#navbar .icons :nth-child(3)");
+
+userIcon.addEventListener("mouseover", function () {
+
+  document.querySelector(".login").classList.remove("d-none");
+
+  let regDiv = document.querySelector(".login");
+
+  regDiv.addEventListener("mouseover", function () {
+
+    this.classList.remove("d-none");
+
+  });
+
+
+
+});
+
+userIcon.addEventListener("mouseout", function () {
+
+  document.querySelector(".login").classList.add("d-none");
+
+  let regDiv = document.querySelector(".login");
+
+  regDiv.addEventListener("mouseout", function () {
+
+    this.classList.add("d-none");
+
+  });
+
+})
+
+
+
+
 $('.slider').slick({
   dots: false,
   infinite: true,
@@ -8,10 +65,10 @@ $('.slider').slick({
   arrows: false
 });
 
-$(".slider").on("click", function(){
-    $(".slider").slick("slickPlay");
+$(".slider").on("click", function () {
+  $(".slider").slick("slickPlay");
 })
 
-$(".slider").on("mouseover", function(){
+$(".slider").on("mouseover", function () {
   $(".slider").slick("slickPlay");
 })
